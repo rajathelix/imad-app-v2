cd5b4d37b75a8199ea46b6c6ff83ec06c53b8d71
rajathelix <rajatmishra.bestfriend@gmail.com>
2017-03-16 13:41:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-app.get('/article one',function(req,res){
+app.get('/article-one',function(req,res){
     res.send('Article one will be served here');
 });
-app.get('/article two',function(req,res){
+app.get('/article-two',function(req,res){
     res.send('Article two will be served here');
 });
-app.get('/article three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('Article three will be served here');
 });
 app.get('/', function (req, res) {
